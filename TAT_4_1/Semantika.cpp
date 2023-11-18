8dabc936f56155209cd97a106e85a99cc735450b
Пользователь <@HOME-I5>
2023-11-18 11:46:42 +0700

    From Big PC

--- TAT_4_1/Semantika.cpp
+++ TAT_4_1/Semantika.cpp
@@ -147,7 +147,7 @@ Tree* Tree::SemInclude(LEX a, OBJ_TYPE ot, DATA_TYPE t)
 	{
 		Tree* root = FindRoot();
 		root->Print();
-		scan->PrintError("Повторное описание идентификатора", a, '\0');
+		scan->PrintError("Повторное описание идентификатора");
 	}
 
 	Tree* v;
@@ -187,21 +187,21 @@ Tree* Tree::SemGetVar(LEX a)
 	{
 		Tree* root = FindRoot();
 		root->Print();
-		scan->PrintError("Отсутствует описание идентификатора", a, '\0');
+		scan->PrintError("Отсутствует описание идентификатора");
 	}
 
 	if (v->node->objType == ObjFunct)
 	{
 		Tree* root = FindRoot();
 		root->Print();
-		scan->PrintError("Неверное использование имени функции", a, '\0');
+		scan->PrintError("Неверное использование имени функции");
 	}
 
 	if (v->node->objType == ObjClass)
 	{
 		Tree* root = FindRoot();
 		root->Print();
-		scan->PrintError("Неверное использование имени класса", a, '\0');
+		scan->PrintError("Неверное использование имени класса");
 	}
 
 	return v;
@@ -232,14 +232,14 @@ Tree* Tree::SemGetFunct(LEX a)
 	{
 		Tree* root = FindRoot();
 		root->Print();
-		scan->PrintError("Отсутствует описание функции", a, '\0');
+		scan->PrintError("Отсутствует описание функции");
 	}
 
 	if (v->node->objType != ObjFunct)
 	{
 		Tree* root = FindRoot();
 		root->Print();
-		scan->PrintError("Идентификатор не является именем функции", a, '\0');
+		scan->PrintError("Идентификатор не является именем функции");
 	}
 
 	return v;
@@ -253,14 +253,14 @@ Tree* Tree::SemGetClass(LEX a)
 	{
 		Tree* root = FindRoot();
 		root->Print();
-		scan->PrintError("Отсутствует описание класса", a, '\0');
+		scan->PrintError("Отсутствует описание класса");
 	}
 
 	if (v->node->objType != ObjClass)
 	{
 		Tree* root = FindRoot();
 		root->Print();
-		scan->PrintError("Идентификатор не является именем класса", a, '\0');
+		scan->PrintError("Идентификатор не является именем класса");
 	}
 
 	return v;
